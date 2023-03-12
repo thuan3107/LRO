eaef453eeb143c60309e2feb0a7fb6f3883f1f3a
Van Thinh Tran <74719505+ooothinhooo@users.noreply.github.com>
2023-03-12 20:46:27 +0700

    Revert "update "

--- admin/src/components/Display/Layout.jsx
+++ admin/src/components/Display/Layout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Content from "./Content.jsx";
-import Slidebar from "./Slidebar";
+import Navbar from "./Navbar.jsx";
 
 function Layout() {
   return (
     <>
       <div class="antialiased bg-white/90 w-full min-h-screen text-slate-300 relative py-2">
-        <div class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl">
-          <Slidebar />
+        <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-2 lg:gap-2 xl:gap-2 w-[97%] ">
+          <Navbar />
           <Content />
         </div>
       </div>
