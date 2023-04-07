a9454fcfba7504adb7afbf12a88521f72a6cbc40
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-04-07 22:16:33 +0700

    update

--- client/src/components/Display/Header.jsx
+++ client/src/components/Display/Header.jsx
@@ -432,7 +432,7 @@ function Header() {
                                   id="handlerLogout"
                                   className={classNames(
                                     active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
+                                    "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
                                   )}
                                 >
                                   Đăng Xuất
